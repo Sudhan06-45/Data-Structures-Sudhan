e2d0bfeea95196aa7ee6a3d80e19fdf7ddfb6731
Sudhan06-45 <Sudhan.Suresh@WinWire.com>
2025-09-18 13:51:55 +0530

    sort

---
+++ ArraysWW/sorting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sorting {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array:");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        
+
+
+    }
+}
